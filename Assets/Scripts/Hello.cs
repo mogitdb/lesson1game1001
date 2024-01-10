3ad643e416f5e16e3ee1ec7024a241fbc32b6b2c
mogitdb <MechaOphidian@gmail.com>
2024-01-10 13:38:50 -0500

    Add project files.

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
